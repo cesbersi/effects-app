f11920193fe1369eae6409047ed55730db64b473
Cesar Bernal <cesbersi@gmail.com>
2018-08-13 12:47:56 -0500

    version inicial

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,19 @@
+export class Usuario {
+
+    /*public id: number;
+    public firstName: string;
+    public lastName: string;
+    public avatar: string;*/
+
+    constructor(
+        public id: number,
+        public first_name: string,
+        public last_name: string,
+        public avatar: string
+    ) {
+        /*this.id = id;
+        this.firstName = first_name;
+        this.avatar = avatar;
+        this.lastName = last_name;*/
+    }
+}
---
+++ src/app/usuarios/lista/lista.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../models/usuario.model';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styles: []
+})
+export class ListaComponent implements OnInit {
+
+  usuarios: Usuario[] = [];
+
+  constructor(private usuarioService: UsuarioService) { }
+
+  ngOnInit() {
+
+    this.usuarioService.getUsers().subscribe(
+      data => {
+        this.usuarios = data;
+      }
+    );
+  }
+
+}
